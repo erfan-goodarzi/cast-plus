6b789f6132bdb046ad8e61e9b6c11fc797a99b7c
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-01-25 19:07:02 +0330

    fix: change podcast detail page layout

--- src/pages/PodcastDetails.tsx
+++ src/pages/PodcastDetails.tsx
@@ -23,81 +23,79 @@ export const PodcastDetails = () => {
   const { data } = useGetPodcastById(parseInt(podcastId));
 
   return (
-    <Container>
-      <Row align='flex-start'>
-        <Col span={2}>
-          <img
-            style={{
-              borderRadius: '10px',
-              width: 220,
+    <Row css={{pl: 28}}>
+      <Col span={2}>
+        <img
+          style={{
+            borderRadius: '10px',
+            width: 220,
+          }}
+          src={data?.feed.artwork}
+        />
+      </Col>
+      <Col>
+        <Text
+          css={{
+            fontSize: '32px',
+            color: '#fff',
+            fontWeight: 'bold',
+            mx: 'auto',
+            textAlign: 'center',
+            '@lg': {
+              mx: 'unset',
+              textAlign: 'left',
+            },
+          }}>
+          {data?.feed.title}
+        </Text>
+        <Text size='larger' css={{ color: '#b6b6b6', lineHeight: '2.1' }}>
+          Made By
+          <Badge
+            css={{
+              ml: 10,
+              fontSize: 16,
+              background: 'transparent',
+              color: 'rgb(255 214 10)',
             }}
-            src={data?.feed.artwork}
-          />
-        </Col>
-        <Col>
-          <Text
-            css={{
-              fontSize: '32px',
-              color: '#fff',
-              fontWeight: 'bold',
-              mx: 'auto',
-              textAlign: 'center',
-              '@lg': {
-                mx: 'unset',
-                textAlign: 'left',
-              },
-            }}>
-            {data?.feed.title}
-          </Text>
-          <Text size='larger' css={{ color: '#b6b6b6', lineHeight: '2.1' }}>
-            Made By
-            <Badge
+            isSquared
+            variant='bordered'>
+            {data?.feed.author}
+          </Badge>
+        </Text>
+        <Text
+          css={{
+            width: '80%',
+            color: '#d1d1d1',
+            fontWeight: 600,
+          }}>
+          {showMore
+            ? data?.feed.description
+            : `${data?.feed.description.substring(0, 360)}`}
+          ...
+        </Text>
+        <Spacer />
+        <Grid.Container css={{ columnGap: 26 }}>
+          <Grid>
+            <Button
+              bordered
+              iconRight={<FontAwesomeIcon icon={faBell} />}
               css={{
-                ml: 10,
-                fontSize: 16,
-                background: 'transparent',
-                color: 'rgb(255 214 10)',
+                color: '#fff',
+                borderColor: '#fff',
+                borderRadius: 3,
               }}
-              isSquared
-              variant='bordered'>
-              {data?.feed.author}
-            </Badge>
-          </Text>
-          <Text
-            css={{
-              width: '80%',
-              color: '#d1d1d1',
-              fontWeight: 600,
-            }}>
-            {showMore
-              ? data?.feed.description
-              : `${data?.feed.description.substring(0, 360)}`}
-            ...
-          </Text>
-          <Spacer />
-          <Grid.Container css={{ columnGap: 26 }}>
-            <Grid>
-              <Button
-                bordered
-                iconRight={<FontAwesomeIcon icon={faBell} />}
-                css={{
-                  color: '#fff',
-                  borderColor: '#fff',
-                  borderRadius: 3,
-                }}
-                auto>
-                Subscribe
-              </Button>
-            </Grid>
-            <Grid>
-              <ShareButton
-                podcastTitle={data?.feed.title!}
-                shareUrl={data?.feed.originalUrl!}
-              />
-            </Grid>
-          </Grid.Container>
-        </Col>
-      </Row>
-    </Container>
+              auto>
+              Subscribe
+            </Button>
+          </Grid>
+          <Grid>
+            <ShareButton
+              podcastTitle={data?.feed.title!}
+              shareUrl={data?.feed.originalUrl!}
+            />
+          </Grid>
+        </Grid.Container>
+      </Col>
+    </Row>
   );
 };
