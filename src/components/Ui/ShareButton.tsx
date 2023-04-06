e4f3ba3cd61fed575673b151aed31fdd32320abe
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-04-06 11:27:19 +0330

    change message of share button

--- src/components/Ui/ShareButton.tsx
+++ src/components/Ui/ShareButton.tsx
@@ -44,23 +44,23 @@ export const ShareButton = ({ podcastTitle, shareUrl }: ShareButtonProps) => {
           With Your Friend
         </Text>
         <Row css={{ gap: 25, pt: 10 }}>
-          {/*FIXNAME: this part should change later. it's shit. */}
-          <FacebookShareButton url={shareUrl} quote={podcastTitle}>
+          {/*FIXME: this part should change later. it's a shit code.*/}
+          <FacebookShareButton url={shareUrl} quote={`Title: ${podcastTitle}`}>
             <FacebookIcon size={32} round />
           </FacebookShareButton>
-          <TwitterShareButton url={shareUrl} title={podcastTitle}>
+          <TwitterShareButton url={shareUrl} title={`Title: ${podcastTitle}`}>
             <TwitterIcon size={32} round />
           </TwitterShareButton>
-          <TelegramShareButton url={shareUrl} title={podcastTitle}>
+          <TelegramShareButton url={shareUrl} title={`Title: ${podcastTitle}`}>
             <TelegramIcon size={32} round />
           </TelegramShareButton>
-          <WhatsappShareButton url={shareUrl} title={podcastTitle}>
+          <WhatsappShareButton url={shareUrl} title={`Title: ${podcastTitle}`}>
             <WhatsappIcon size={32} round />
           </WhatsappShareButton>
-          <LinkedinShareButton url={shareUrl} title={podcastTitle}>
+          <LinkedinShareButton url={shareUrl} title={`Title: ${podcastTitle}`}>
             <LinkedinIcon size={32} round />
           </LinkedinShareButton>
-          <TumblrShareButton url={shareUrl} title={podcastTitle}>
+          <TumblrShareButton url={shareUrl} title={`Title: ${podcastTitle}`}>
             <TumblrIcon size={32} round />
           </TumblrShareButton>
         </Row>
