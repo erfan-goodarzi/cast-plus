8570768b904b56b49e862dc4ecae0a2fee0efc22
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-04-13 17:05:07 +0330

    chore: implement load more button

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,10 +1,17 @@
-import { Card, Container, Grid, Text } from '@nextui-org/react';
+import { Button, Card, Container, Grid, Text } from '@nextui-org/react';
+import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { useGetCategories } from '../api';
 
 export const Categories = () => {
   const { data } = useGetCategories();
-  console.log(data)
+  const ITEMS_INCREMENT = 15;
+  const [itemsToShow, setItemsToShow] = useState(ITEMS_INCREMENT);
+
+  const onLoadMore = () => {
+    setItemsToShow((prevState) => prevState + ITEMS_INCREMENT);
+  };
+
   return (
     <Container css={{ mt: 30, height: '81vh', overflow: 'hidden' }}>
       <Tabs className='react-tabs'>
@@ -18,14 +25,24 @@ export const Categories = () => {
                 background: 'transparent',
               }}>
               <TabList>
-                {data?.feeds.slice(0, 30).map((c) => (
+                {data?.feeds.slice(0, itemsToShow).map((c) => (
                   <Tab key={c.id}>{c.name}</Tab>
                 ))}
+                <Button
+                  onClick={onLoadMore}
+                  css={{
+                    borderRadius: 1,
+                    background: 'transparent',
+                    color: '$primary',
+                    borderTop: '1px solid #fff',
+                  }}>
+                  See More
+                </Button>
               </TabList>
             </Card>
           </Grid>
           <Grid>
-            {data?.feeds.slice(0, 30).map((c) => (
+            {data?.feeds.slice(0, itemsToShow).map((c) => (
               <>
                 <TabPanel key={c.id}>
                   <Text color='#fff'>All Episodes About:</Text>
