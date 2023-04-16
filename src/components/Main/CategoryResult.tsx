61aca3df930b31624d4bb86999a79410c9d65906
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-04-16 16:13:18 +0330

    chore: integrate category result with api

--- src/components/Main/CategoryResult.tsx
+++ src/components/Main/CategoryResult.tsx
@@ -1,11 +1,23 @@
-import { Badge, Button, Card, Col, Grid, Row, Text } from '@nextui-org/react';
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Grid,
+  Loading,
+  Row,
+  Text,
+} from '@nextui-org/react';
 import { PIApiCategory } from 'podcastindexjs/lib/types';
+import { useGetTopPodcasts } from '../../api';
 
 interface Props {
   category: PIApiCategory;
 }
 
-export const CategoryResult = (category: Props) => {
+export const CategoryResult = ({ category }: Props) => {
+  const { data, isLoading } = useGetTopPodcasts(category.name);
+
   return (
     <>
       <Grid.Container css={{ gap: 12, mb: 20 }}>
@@ -23,59 +35,67 @@ export const CategoryResult = (category: Props) => {
             }}
             isSquared
             variant='bordered'>
-            {category.category.name}
+            {category.name}
           </Badge>
         </Grid>
       </Grid.Container>
-      <Row css={{ gap: 40 }}>
-        <Col span={4}>
-          <Card css={{ bg: 'none', borderRadius: '$xs' }}>
-            <Card.Body css={{ p: 0 }}>
-              <Card.Image
-                src='https://image.simplecastcdn.com/images/48d244e8-5815-45c1-9b60-b03aceaa2ad5/903d31c7-f87f-4198-9eaa-bc95274c6fdb/3000x3000/bx5480-moodys-talks-inside-economics-simplecastfinal.jpg?aid=rss_feed'
-                objectFit='cover'
-                width='100%'
-                height='100%'
-                alt='Relaxing app background'
-              />
-            </Card.Body>
-            <Card.Footer
-              isBlurred
-              css={{
-                borderRadius: 0,
-                position: 'absolute',
-                bgBlur: '#0f111466',
-                borderTop: '$borderWeights$light solid $gray800',
-                bottom: -2,
-                zIndex: 1,
-              }}>
-              <Row align='baseline'>
-                <Col span={8}>
-                  <Text color='#fff' weight='bold' size={14}>
-                    Breathing App
-                  </Text>
-                </Col>
-                <Col span={4}>
-                  <Row justify='flex-end'>
-                    <Button
-                      size='sm'
-                      auto
-                      bordered
-                      css={{ bg: '$primaryLight', borderRadius: '$xs' }}>
-                      <Text
-                        css={{ color: 'inherit', letterSpacing: 0.4 }}
-                        weight='bold'
-                        size={13}
-                        transform='uppercase'>
-                        Listen
-                      </Text>
-                    </Button>
-                  </Row>
-                </Col>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Col>
+      <Row wrap='wrap' css={{ gap: 40 }}>
+        {isLoading ? (
+          <Loading css={{ ml: 'auto' }} />
+        ) : (
+          data?.feeds.map(
+            (pod: { id: number; image: string; title: string }) => (
+              <Col span={2} key={pod.id}>
+                <Card css={{ bg: 'none', borderRadius: '$xs' }}>
+                  <Card.Body css={{ p: 0 }}>
+                    <Card.Image
+                      src={pod.image}
+                      objectFit='cover'
+                      width='100%'
+                      height='100%'
+                      alt={pod.title}
+                    />
+                  </Card.Body>
+                  <Card.Footer
+                    isBlurred
+                    css={{
+                      borderRadius: 0,
+                      position: 'absolute',
+                      bgBlur: '#0f111466',
+                      borderTop: '$borderWeights$light solid $gray800',
+                      bottom: -2,
+                      zIndex: 1,
+                    }}>
+                    <Row align='baseline'>
+                      <Col span={8}>
+                        <Text color='#fff' weight='bold' size={14}>
+                          {pod.title}
+                        </Text>
+                      </Col>
+                      <Col span={4}>
+                        <Row justify='flex-end'>
+                          <Button
+                            size='sm'
+                            auto
+                            bordered
+                            css={{ bg: '$primaryLight', borderRadius: '$xs' }}>
+                            <Text
+                              css={{ color: 'inherit', letterSpacing: 0.4 }}
+                              weight='bold'
+                              size={13}
+                              transform='uppercase'>
+                              Listen
+                            </Text>
+                          </Button>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            )
+          )
+        )}
       </Row>
     </>
   );
