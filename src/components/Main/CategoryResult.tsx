390a8081b7204554075e177c74ffa1ba42b20607
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-04-19 15:02:04 +0330

    fix: fix button reload issue

--- src/components/Main/CategoryResult.tsx
+++ src/components/Main/CategoryResult.tsx
@@ -8,6 +8,10 @@ import {
   Row,
   Text,
 } from '@nextui-org/react';
+import {
+  Link,
+  useNavigate,
+} from '@tanstack/react-location';
 import { PIApiCategory } from 'podcastindexjs/lib/types';
 import { useGetTopPodcasts } from '../../api';
 import DefaultImg from '../../assets/default-img.jpg';
@@ -18,6 +22,7 @@ interface Props {
 
 export const CategoryResult = ({ category }: Props) => {
   const { data, isLoading } = useGetTopPodcasts(category.name);
+  const navigate = useNavigate();
 
   const handleImgError = (e: React.SyntheticEvent<HTMLImageElement>) => {
     e.currentTarget.src = DefaultImg;
@@ -51,53 +56,73 @@ export const CategoryResult = ({ category }: Props) => {
           data?.feeds.map(
             (pod: { id: number; image: string; title: string }) => (
               <Col span={2} key={pod.id}>
-                <Card css={{ bg: 'none', borderRadius: '$xs' }}>
-                  <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                      onError={handleImgError}
-                      src={pod.image}
-                      objectFit='cover'
-                      width='100%'
-                      height='100%'
-                      alt={pod.title}
-                    />
-                  </Card.Body>
-                  <Card.Footer
-                    isBlurred
-                    css={{
-                      borderRadius: 0,
-                      position: 'absolute',
-                      bgBlur: '#0f111466',
-                      borderTop: '$borderWeights$light solid $gray800',
-                      bottom: -2,
-                      zIndex: 1,
-                    }}>
-                    <Row align='baseline'>
-                      <Col span={8}>
-                        <Text color='#fff' weight='bold' size={14}>
-                          {pod.title.substring(0, 20)}
-                        </Text>
-                      </Col>
-                      <Col span={4}>
-                        <Row justify='flex-end'>
-                          <Button
-                            size='sm'
-                            auto
-                            bordered
-                            css={{ bg: '$primaryLight', borderRadius: '$xs' }}>
-                            <Text
-                              css={{ color: 'inherit', letterSpacing: 0.4 }}
-                              weight='bold'
-                              size={13}
-                              transform='uppercase'>
-                              Listen
-                            </Text>
-                          </Button>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </Card.Footer>
-                </Card>
+                <Link to={`/explore/${pod.id}`}>
+                  <Card css={{ bg: 'none', borderRadius: '$xs' }}>
+                    <Card.Body css={{ p: 0 }}>
+                      <Card.Image
+                        className='hovered-img'
+                        onError={handleImgError}
+                        css={{
+                          transition: 'all .2s ease-in-out',
+                          cursor: 'pointer',
+                          '&:hover': {
+                            transition: 'all .2s ease-in-out',
+                            transform: 'scale(1.2)',
+                          },
+                        }}
+                        src={pod.image}
+                        objectFit='cover'
+                        width='100%'
+                        height='100%'
+                        alt={pod.title}
+                      />
+                    </Card.Body>
+                    <Card.Footer
+                      isBlurred
+                      css={{
+                        borderRadius: 0,
+                        position: 'absolute',
+                        bgBlur: '#0f111466',
+                        borderTop: '$borderWeights$light solid $gray800',
+                        bottom: -2,
+                        zIndex: 1,
+                      }}>
+                      <Row align='baseline'>
+                        <Col span={8}>
+                          <Text color='#fff' weight='bold' size={14}>
+                            {pod.title.substring(0, 20)}
+                          </Text>
+                        </Col>
+                        <Col span={4}>
+                          <Row justify='flex-end'>
+                            <Button
+                              onClick={() => {
+                                navigate({ to: `/explore/${pod.id}` });
+                              }}
+                              size='sm'
+                              auto
+                              bordered
+                              css={{
+                                bg: '$primaryLight',
+                                borderRadius: '$xs',
+                              }}>
+                              <Text
+                                css={{
+                                  letterSpacing: 0.4,
+                                  color: '$primary',
+                                }}
+                                weight='bold'
+                                size={13}
+                                transform='uppercase'>
+                                Listen
+                              </Text>
+                            </Button>
+                          </Row>
+                        </Col>
+                      </Row>
+                    </Card.Footer>
+                  </Card>
+                </Link>
               </Col>
             )
           )
