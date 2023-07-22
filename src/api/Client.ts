59ccd1c4b97a55d93d05f398d84655f88722e395
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-07-22 15:12:22 +0330

    fix: remove unnecessary comments

--- src/api/Client.ts
+++ src/api/Client.ts
@@ -1,16 +1,3 @@
-// import { GraphQLClient } from 'graphql-request';
-// const baseURL = 'https://api.podchaser.com/graphql';
-
-// export const httpClient = (query: string) => {
-//   const graphQLClient = new GraphQLClient(baseURL, {
-//     headers: {
-//       authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-//     },
-//   });
-
-//   return graphQLClient.request(query);
-// };
-
 import { PodcastIndexClient } from 'podcastindexjs';
 
 export const client = new PodcastIndexClient(
--- src/api/query/getTopEpisode.ts
+++ src/api/query/getTopEpisode.ts
@@ -1,34 +1,3 @@
-// import { gql } from 'graphql-request';
-// import { httpClient } from '..';
-// import { useQuery } from 'react-query';
-
-// const getTopEpisode = (): Promise<TopEpisode> => {
-//   const query = gql`
-//     query {
-//       episodes(sort: { sortBy: TRENDING },  first: 5) {
-//         data {
-//           id
-//           title
-//           description
-//           imageUrl
-//           length
-//           podcast {
-//             title
-//             author {
-//               name
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `;
-//   return httpClient(query);
-// };
-
-// export const useGetTopEpisode = () => {
-//   return useQuery<TopEpisode>(`episode`, async () => await getTopEpisode());
-// };
-
 import { client } from '..';
 import { useQuery } from 'react-query';
 
