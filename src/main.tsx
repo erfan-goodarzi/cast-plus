61e9c9901232e74765a3c2198aced2c35f55f86e
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-08-04 08:25:34 +0330

    style: abstract theme object

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,12 @@
-import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
+import { theme } from './style';
 
 const queryClient = new QueryClient();
-const theme = createTheme({
-  type: 'light',
-  theme: {
-    radii: {
-      xs: '5px',
-      base: '10px',
-    },
-    colors: {
-      primary: '#ffd60a',
-      primarySolidContrast: '#0f172b',
-      primaryLightContrast: '#ffd60a',
-      primaryLight: '#00254d',
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/style/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@nextui-org/react';
+
+export const theme = createTheme({
+  type: 'light',
+  theme: {
+    radii: {
+      xs: '5px',
+      base: '10px',
+    },
+    colors: {
+      primary: '#ffd60a !important',
+      primarySolidContrast: '#0f172b',
+      primaryLightContrast: '#ffd60a',
+      primaryLight: '#00254d',
+    },
+  },
+});
